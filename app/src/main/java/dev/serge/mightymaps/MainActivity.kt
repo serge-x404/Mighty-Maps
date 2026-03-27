046f9cde2717dc7c58195e9739e36b305eb2e884
serge-x404 <kpdev1124@gmail.com>
2026-03-27 15:49:58 +0530

    Polygon and Polyline

--- app/src/main/java/dev/serge/mightymaps/MainActivity.kt
+++ app/src/main/java/dev/serge/mightymaps/MainActivity.kt
@@ -1,6 +1,5 @@
 package dev.serge.mightymaps
 
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -12,7 +11,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import androidx.compose.ui.graphics.Color
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -20,6 +19,8 @@ import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.Polygon
+import com.google.maps.android.compose.Polyline
 import com.google.maps.android.compose.rememberCameraPositionState
 
 class MainActivity : ComponentActivity() {
@@ -28,18 +29,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-            val india = LatLng(23.05,72.50)
+            val home = LatLng(20.95,72.92)
 
             val cameraPositonState = rememberCameraPositionState {
                 position = CameraPosition.fromLatLngZoom(
-                    india,
+                    home,
                     4f
                 )
             }
 
             val testingTiltBearing = remember {
                 CameraPosition(
-                    india, // google plex coordinates
+                    home, // google plex coordinates
                     4f,     // close-up view
                     0f,       // rotated 90 degrees
                     0f     // 30 degree angle view
@@ -54,7 +55,7 @@ class MainActivity : ComponentActivity() {
 
             var mapType by remember {
                 mutableStateOf(
-                    MapType.SATELLITE
+                    MapType.NORMAL
                 )
             }
 
@@ -64,6 +65,12 @@ class MainActivity : ComponentActivity() {
 //                applicationContext.resources,
 //                R.drawable.target
 //            )
+
+            val office = LatLng(20.94,72.92)
+            val college = LatLng(23.03,75.50)
+            val school = LatLng(23.04,72.50)
+
+            val routePoints = listOf(office,college, school)
 
             GoogleMap(
                 modifier = Modifier
@@ -75,7 +82,7 @@ class MainActivity : ComponentActivity() {
                 properties = MapProperties(mapType = mapType)
             ) {
                 Marker(
-                    state = MarkerState(india),
+                    state = MarkerState(home),
                     title = "India",
                     snippet = "Marker @ (23.05,72.50)",
                     alpha = 1f,
@@ -84,6 +91,35 @@ class MainActivity : ComponentActivity() {
                         true    // false -> to show marker snippet
                     }
                 )
+                Polyline(
+                    points = routePoints,
+                    clickable = true,
+                    width = 10f,
+                    color = Color.Red,
+                    onClick = {
+                        Log.v("TAGY","You clicked polyline")
+                    },
+                    geodesic = true
+                )
+
+                val trianglePoints = listOf(
+                    LatLng(34.0522,-118.2437), // LA
+                    LatLng(37.7749,-122.4194), // SF
+                    LatLng(32.7157,-117.1611), // SD
+                )
+
+
+                Polygon(
+                    points = trianglePoints,
+                    fillColor = Color.Yellow,
+                    strokeColor = Color.Red,
+                    strokeWidth = 7f,
+                    clickable = true,
+                    onClick = {
+                        Log.v("TAGY","Polygon is clicked")
+                    }
+                )
+
             }
         }
     }
